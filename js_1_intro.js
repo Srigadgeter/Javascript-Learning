1c14cfd4822787c9201c803e647f787770b8dbb5
Srigadgeter <srigadgets@gmail.com>
2018-12-21 10:04:34 +0530

    js intro - js simplification1

--- js_1_intro.js
+++ js_1_intro.js
@@ -1,5 +1,12 @@
+// document.getElementById('btn1').onclick = function(){
+//     document.getElementById('para1').innerHTML = "Yes, I'm Learning JS";
+//     document.getElementById('para1').style.color = "blue";
+//     document.getElementById('para1').style.fontSize = "20px";
+// }
+
 document.getElementById('btn1').onclick = function(){
-    document.getElementById('para1').innerHTML = "Yes, I'm Learning JS";
-    document.getElementById('para1').style.color = "blue";
-    document.getElementById('para1').style.fontSize = "20px";
+    var para1 = document.getElementById('para1')
+    para1.innerHTML = "Yes, I'm Learning JS";
+    para1.style.color = "blue";
+    para1.style.fontSize = "20px";
 }
