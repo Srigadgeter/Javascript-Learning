a160b2d98f8f4110c53fdbca8fe5d09154be3b66
Srigadgeter <srigadgets@gmail.com>
2018-12-21 10:01:02 +0530

    js intro

---
+++ js_1_intro.js
@@ -0,0 +1,5 @@
+document.getElementById('btn1').onclick = function(){
+    document.getElementById('para1').innerHTML = "Yes, I'm Learning JS";
+    document.getElementById('para1').style.color = "blue";
+    document.getElementById('para1').style.fontSize = "20px";
+}
