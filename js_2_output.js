f5bca1c2c9b6fd181a8f1f45e78e3ea009d5ba8d
Srigadgeter <srigadgets@gmail.com>
2018-12-21 10:38:46 +0530

    js outputs

---
+++ js_2_output.js
@@ -0,0 +1,14 @@
+// alert
+window.alert("Concatenation will be done when two numbers add up with string = "+3+3)
+window.alert(3+3+"abcd")
+window.alert(3+3)
+
+// document.write
+document.write(123+123)
+
+// innerHTML
+document.getElementById("para").innerHTML = "abcd"
+
+// console.log
+console.log(123 + 123)
+console.log("abcd"+3+3)
