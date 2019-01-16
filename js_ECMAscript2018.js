d617b076e451a4e9037bea764e247314d55499dd
Srigadgeter <srigadgets@gmail.com>
2019-01-16 16:57:39 +0530

    js_ECMAscript2018

---
+++ js_ECMAscript2018.js
@@ -0,0 +1,79 @@
+// ECMAscript 2018
+
+
+// Object rest/spread property
+person = {
+    firstName: 'raj',
+    x: 2,
+    lastName: 'kumar',
+    age: 20,
+    email: 'rajkumar@gmail.com',
+    y: 5
+}
+
+const {x, y, ...z} = person
+// Rest element must be last element
+// Rest element ==> ...z
+
+console.log("Object rest/spread property concept")
+console.log(x)
+console.log(y)
+console.log(z)
+
+// ---------------------------------------
+
+// Promise
+// https://www.youtube.com/watch?v=s6SH72uAn3Q - youtube video link to learn promise concept in ecmascript2018
+console.log("Promise : simple concept")
+
+promiseToCleanTheHouse = new Promise(function(resolve, reject) {
+    let isHouseClean = false
+
+    isHouseClean ? resolve('clean') : reject('not clean')
+})
+
+promiseToCleanTheHouse.then(function(paramsFromResolveCall) {
+    console.log('The house is ' + paramsFromResolveCall)
+}).catch(function(paramsFromRejectCall) {
+    console.log('The house is ' + paramsFromRejectCall)
+})
+
+console.log("Promise : complex concept")
+
+cleanHouse = function() {
+    return new Promise(function(resolve, reject) {
+        resolve('House is cleaned.')
+    })
+}
+
+removeGarbage = function(message) {
+    return new Promise(function(resolve, reject) {
+        resolve(message + ' The garbage is removed.')
+    })
+}
+
+winChocolate = function(message) {
+    return new Promise(function(resolve, reject) {
+        resolve(message + ' You won chocolate.')
+    })
+}
+
+cleanHouse().then(function(msgFromResolve) {
+    removeGarbage(msgFromResolve).then(function(msgFromResolve) {
+        winChocolate(msgFromResolve).then(function(msgFromResolve) {
+            console.log('You have finished all. ' + msgFromResolve)
+        })
+    })
+})
+
+// above lines could write aslike this
+
+cleanHouse().then(function(msgFromResolve) {
+    return removeGarbage(msgFromResolve)
+}).then(function(msgFromResolve) {
+    return winChocolate(msgFromResolve)
+}).then(function(msgFromResolve) {
+    console.log('You have finished all. ' + msgFromResolve)
+})
+
+// ---------------------------------------
